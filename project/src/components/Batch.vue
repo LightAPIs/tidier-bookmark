01f7231fddf6f504a91efae823798b81aadc8c1a
Light <47911535+LightAPIs@users.noreply.github.com>
2020-12-12 17:39:52 +0800

    feat(Batch.vue,messages.json,variables.less,manifest.*.json): 完成批量操作组件
    完成了批量操作的路由组件；
    为扩展程序添加了访问图标缓存的权限

---
+++ project/src/components/Batch.vue
@@ -0,0 +1,203 @@
+<template>
+  <div id="batch-component">
+    <a-affix :offset-top="0">
+      <a-config-provider :auto-insert-space-in-button="false">
+        <a-space class="top-button-group">
+          <a-button @click="onPreview">
+            {{ $ui.get('batchPreviewText') }}
+          </a-button>
+          <a-button v-show="effectVisible" :disabled="checkedkeys.length === 0" type="primary" @click="onEffect">
+            {{ $ui.get('batchEffectText') }}
+          </a-button>
+        </a-space>
+      </a-config-provider>
+    </a-affix>
+    <a-tree
+      :tree-data="treeData"
+      :expanded-keys="expandedKeys"
+      :replace-fields="replaceFields"
+      show-icon
+      show-line
+      checkable
+      block-node
+      :selectable="false"
+      @expand="onExpand"
+      @check="onCheck"
+    >
+      <template slot="folder">
+        <a-icon type="folder" theme="filled" />
+      </template>
+      <template slot="favicon" slot-scope="{ url }">
+        <img :src="'chrome://favicon/' + url" />
+      </template>
+      <template slot="replace" slot-scope="{ title, url, newTitle, matchIndex }">
+        <a-tooltip placement="topLeft" class="replace-title" overlay-class-name="tips">
+          <template slot="title">
+            <p>{{ $ui.get('bookmarkUrlLabel', ': ') + url }}</p>
+            <p>{{ $ui.get('bookmarkOriginalNameLabel', ': ') + title }}</p>
+            <p>{{ $ui.get('batchNewNameLabel', ': ') + newTitle }}</p>
+            <p>
+              {{ $ui.get('rulesTestMatchLabel', ': ') + matchIndex.join(' ') }}
+            </p>
+          </template>
+          <a-space>
+            <span>
+              {{ title }}
+            </span>
+            <span class="arrow">
+              ==>
+            </span>
+            <span>
+              {{ newTitle }}
+            </span>
+          </a-space>
+        </a-tooltip>
+      </template>
+      <template slot="empty">
+        -
+      </template>
+    </a-tree>
+  </div>
+</template>
+
+<script>
+const replaceFields = {
+  key: 'id',
+};
+
+export default {
+  name: 'Batch',
+  data() {
+    return {
+      effectVisible: false,
+      expandedKeys: [],
+      checkedkeys: [],
+      treeData: [],
+      rulesData: [],
+      replaceFields,
+    };
+  },
+  methods: {
+    onPreview() {
+      chrome.storage.local.get('rules', result => {
+        this.rulesData = result.rules;
+        chrome.bookmarks.getTree(arr => {
+          this.treeData = this.handleArray(arr[0].children);
+          this.expandedKeys = this.treeData.map(item => {
+            return item.id;
+          });
+          this.effectVisible = this.treeData.length > 0;
+        });
+      });
+    },
+    onEffect() {
+      const { treeData } = this;
+      this.traverseArray(treeData);
+      this.$message.success(this.$ui.get('batchCompletionText'), 2);
+      this.onPreview();
+    },
+    onCheck(checkedkeys) {
+      this.checkedkeys = checkedkeys;
+    },
+    onExpand(expandedKeys) {
+      this.expandedKeys = expandedKeys;
+    },
+    /**
+     * 递归遍历处理数组节点的方法
+     * @param {Array} arr 数组值
+     */
+    handleArray(arr) {
+      return arr
+        .map(item => {
+          if (item.children) {
+            item.children = this.handleArray(item.children);
+            item.scopedSlots = {
+              icon: 'folder',
+            };
+            if (item.children.length > 0) {
+              return item;
+            }
+            return null;
+          } else {
+            const { title } = item;
+            const matchIndex = [];
+            let result = title;
+            this.rulesData.forEach(rule => {
+              const { enable, pattern, flags, replacement, index } = rule;
+              if (enable) {
+                if (this.$ui.regTest(pattern, flags, result)) {
+                  matchIndex.push(index);
+                  result = this.$ui.regReplace(pattern, flags, result, replacement);
+                }
+              }
+            });
+
+            if (matchIndex.length > 0) {
+              item.newTitle = result;
+              item.matchIndex = [...matchIndex];
+              item.scopedSlots = {
+                icon: 'favicon',
+                switcherIcon: 'empty',
+                title: 'replace',
+              };
+              return item;
+            }
+            return null;
+          }
+        })
+        .filter(v => v);
+    },
+    /**
+     * 递归遍历更改书签节点的方法
+     * @param {Array} arr 数组节点值
+     */
+    traverseArray(arr) {
+      if (this.checkedkeys.length > 0) {
+        arr.forEach(item => {
+          if (this.checkedkeys.length > 0) {
+            if (item.children) {
+              this.traverseArray(item.children);
+            } else {
+              if (this.checkedkeys.includes(item.id)) {
+                this.checkedkeys = this.checkedkeys.filter(v => v !== item.id);
+                chrome.bookmarks.update(
+                  item.id,
+                  {
+                    title: item.newTitle,
+                  },
+                  res => {
+                    if (!res) {
+                      this.$message.error(this.$ui.get('bookmarkErrorUpdateText'), 2);
+                      console.error(item);
+                    }
+                  }
+                );
+              }
+            }
+          }
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="less">
+#batch-component {
+  .top-button-group {
+    background-color: @general-background-color;
+    width: 100%;
+    padding: 10px;
+  }
+}
+.replace-title {
+  width: 100%;
+  overflow: hidden;
+}
+.arrow {
+  color: @arrow-color;
+}
+.tips {
+  word-break: break-all;
+}
+</style>
