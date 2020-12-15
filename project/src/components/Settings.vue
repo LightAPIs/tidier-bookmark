a7df07cfc79f28346a49065e30604fca4278cd1f
Light <47911535+LightAPIs@users.noreply.github.com>
2020-12-15 16:19:57 +0800

    feat(Settings.vue,messages.json): 完成通用设置路由组件

---
+++ project/src/components/Settings.vue
@@ -0,0 +1,155 @@
+<template>
+  <div id="settings-component">
+    <a-list item-layout="horizontal" :data-source="bookmarkData">
+      <h3 slot="header">
+        {{ $ui.get('settingsBookmarkLabel') }}
+      </h3>
+      <a-list-item slot="renderItem" slot-scope="item">
+        <div class="align-items-center">
+          <a-switch :data-id="item.key" @change="onSwitchChange" :checked="item.value" />
+          <span class="item-text">
+            {{ item.title }}
+          </span>
+        </div>
+      </a-list-item>
+    </a-list>
+    <a-list item-layout="horizontal" :data-source="skinData">
+      <h3 slot="header">
+        {{ $ui.get('settingsSkinLabel') }}
+      </h3>
+      <a-list-item slot="renderItem" slot-scope="item">
+        <div class="align-items-center">
+          <span class="label">
+            {{ item.title }}
+          </span>
+          <a-select :value="item.value" @change="onSelectChange" class="select">
+            <a-select-option v-for="option in item.options" :key="option.value" :value="option.value">
+              {{ option.name }}
+            </a-select-option>
+          </a-select>
+        </div>
+      </a-list-item>
+    </a-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+  data() {
+    return {
+      bookmarkData: [
+        {
+          key: 'bookmarkOnTop',
+          value: false,
+          title: this.$ui.get('settingsBookmarkOnTopText'),
+        },
+        {
+          key: 'folderOnTop',
+          value: false,
+          title: this.$ui.get('settingsFolderOnTopText'),
+        },
+      ],
+      skinData: [
+        {
+          key: 'darkMode',
+          value: 0,
+          title: this.$ui.get('settingsDarkModeText', ':'),
+          options: [
+            {
+              name: this.$ui.get('settingsDarkModeOptionsDefault'),
+              value: 0,
+            },
+            {
+              name: this.$ui.get('settingsDarkModeOptionsEnabled'),
+              value: 1,
+            },
+            {
+              name: this.$ui.get('settingsDarkModeOptionsDisabled'),
+              value: -1,
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    onSwitchChange(checked, event) {
+      const { id } = event.target.dataset;
+      this.bookmarkData.forEach((item, index) => {
+        if (item.key == id) {
+          this.bookmarkData[index].value = checked;
+        }
+      });
+      this.setValue();
+    },
+    onSelectChange(value) {
+      this.skinData[0].value = value;
+      this.setValue();
+    },
+    /**
+     * 存储设置的方法
+     */
+    setValue() {
+      const settings = [];
+      this.bookmarkData.forEach(data => {
+        settings.push({
+          key: data.key,
+          value: data.value,
+        });
+      });
+      this.skinData.forEach(skin => {
+        settings.push({
+          key: skin.key,
+          value: skin.value,
+        });
+      });
+      chrome.storage.local.set(
+        {
+          settings,
+        },
+        () => {
+          console.log('The settings have been set.');
+        }
+      );
+    },
+  },
+  created() {
+    chrome.storage.local.get('settings', result => {
+      const settings = result.settings || [];
+      this.bookmarkData.forEach((data, index) => {
+        settings.forEach(setting => {
+          if (setting.key === data.key) {
+            this.bookmarkData[index].value = setting.value;
+          }
+        });
+      });
+      this.skinData.forEach((skin, index) => {
+        settings.forEach(setting => {
+          if (setting.key === skin.key) {
+            this.skinData[index].value = setting.value;
+          }
+        });
+      });
+    });
+  },
+};
+</script>
+
+<style lang="less">
+#settings-component {
+  .align-items-center {
+    display: flex;
+    align-items: center;
+    .item-text {
+      margin-left: 15px;
+    }
+  }
+  .select {
+    width: 120px;
+  }
+  .label {
+    margin-right: 15px;
+  }
+}
+</style>
