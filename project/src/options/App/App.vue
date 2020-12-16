c57b126d11e91033ad1ab3646766f59eb0f02c8e
Light <47911535+LightAPIs@users.noreply.github.com>
2020-12-16 15:42:25 +0800

    feat(opitons,variables.less,messages.json): 完成选项页面框架

--- project/src/options/App/App.vue
+++ project/src/options/App/App.vue
@@ -1,22 +1,130 @@
 <template>
-  <div class="main_app">
-    <h1>Hello Options</h1>
+  <div id="opitons-app">
+    <a-layout>
+      <a-layout-sider v-model="collapsed" :trigger="null" collapsible class="layout-height">
+        <div class="logo">
+          <img src="@/img/icon-24.png" />
+          {{ this.collapsed ? '' : $ui.get('extensionName') }}
+        </div>
+        <a-menu theme="dark" mode="inline" :selected-keys="selectedKeys" @select="handleSelect">
+          <a-menu-item key="1">
+            <router-link to="/settings">
+              <a-icon type="setting" />
+              <span>
+                {{ $ui.get('optionsGeneralSettingsText') }}
+              </span>
+            </router-link>
+          </a-menu-item>
+          <a-menu-item key="2">
+            <router-link to="/rules">
+              <a-icon type="ordered-list" />
+              <span>
+                {{ $ui.get('optionsRuleListText') }}
+              </span>
+            </router-link>
+          </a-menu-item>
+          <a-menu-item key="3">
+            <router-link to="/batch">
+              <a-icon type="file-search" />
+              <span>
+                {{ $ui.get('optionsBatchOperationText') }}
+              </span>
+            </router-link>
+          </a-menu-item>
+          <a-menu-item key="4">
+            <router-link to="/about">
+              <a-icon type="github" />
+              <span>
+                {{ $ui.get('optionsAboutText') }}
+              </span>
+            </router-link>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout class="layout-height layout-overflow">
+        <a-layout-header class="header">
+          <a-icon class="trigger" :type="collapsed ? 'menu-unfold' : 'menu-fold'" @click="triggerSwitch" />
+        </a-layout-header>
+        <a-layout-content class="content">
+          <router-view></router-view>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
   </div>
 </template>
 
 <script>
+const { hash } = location;
+const selectedKeys = [];
+switch (hash) {
+  case '#/rules':
+    selectedKeys.push('2');
+    break;
+  case '#/batch':
+    selectedKeys.push('3');
+    break;
+  case '#/about':
+    selectedKeys.push('4');
+    break;
+  case '#/settings':
+  default:
+    selectedKeys.push('1');
+    break;
+}
+
 export default {
-  name: 'App',
+  data() {
+    return {
+      collapsed: false,
+      selectedKeys,
+    };
+  },
+  methods: {
+    triggerSwitch() {
+      this.collapsed = !this.collapsed;
+    },
+    handleSelect(obj) {
+      const { selectedKeys } = obj;
+      this.selectedKeys = [...selectedKeys];
+    },
+  },
 };
 </script>
 
-<style>
-.main_app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less">
+#opitons-app {
+  .layout-height {
+    height: 100vh;
+  }
+  .layout-overflow {
+    overflow: hidden;
+  }
+  .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+  }
+  .header {
+    padding: 0px;
+    background-color: @page-background-color;
+  }
+  .content {
+    margin: 24px 16px;
+    padding: 24px;
+    min-height: 280px;
+    background-color: @page-background-color;
+    overflow: auto;
+  }
+  .logo {
+    height: 32px;
+    line-height: 32px;
+    font-size: 16px;
+    color: @logo-text-color;
+    text-align: center;
+    background-color: @logo-background-color;
+    margin: 16px;
+  }
 }
 </style>
--- project/src/options/index.js
+++ project/src/options/index.js
@@ -1,11 +1,109 @@
 import Vue from 'vue';
-import AppComponent from './App/App.vue';
+import VueRouter from 'vue-router';
+import App from './App/App.vue';
+import ui from '../commons/ui';
+import {
+  Affix,
+  Button,
+  Checkbox,
+  Col,
+  ConfigProvider,
+  Divider,
+  Empty,
+  Form,
+  Icon,
+  Input,
+  InputNumber,
+  Layout,
+  List,
+  Menu,
+  message,
+  Modal,
+  Popconfirm,
+  Row,
+  Select,
+  Space,
+  Switch,
+  Table,
+  Tag,
+  Timeline,
+  Tooltip,
+  Tree,
+} from 'ant-design-vue';
+import Settings from '../components/Settings.vue';
+import Rules from '../components/Rules.vue';
+import Batch from '../components/Batch.vue';
+import About from '../components/About.vue';
 
-Vue.component('app-component', AppComponent);
+Vue.use(VueRouter);
+Vue.use(Layout);
+Vue.use(Menu);
+Vue.use(Icon);
+Vue.use(Table);
+Vue.use(Popconfirm);
+Vue.use(Button);
+Vue.use(Switch);
+Vue.use(ConfigProvider);
+Vue.use(Form);
+Vue.use(Input);
+Vue.use(Modal);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Checkbox);
+Vue.use(Space);
+Vue.use(InputNumber);
+Vue.use(Tooltip);
+Vue.use(Tag);
+Vue.use(Tree);
+Vue.use(Affix);
+Vue.use(List);
+Vue.use(Empty);
+Vue.use(Select);
+Vue.use(Divider);
+Vue.use(Timeline);
+Vue.prototype.$message = message;
+Vue.prototype.$ui = ui;
+Vue.config.productionTip = false;
+
+const routes = [
+  {
+    path: '/settings',
+    name: 'Settings',
+    component: Settings,
+  },
+  {
+    path: '/rules',
+    name: 'Rules',
+    component: Rules,
+  },
+  {
+    path: '/batch',
+    name: 'Batch',
+    component: Batch,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+  {
+    path: '*',
+    redirect: '/settings',
+  },
+];
+const router = new VueRouter({
+  routes,
+});
+
+document.title = ui.get('optionsTitle');
 
 new Vue({
-  el: '#app',
-  render: createElement => {
-    return createElement(AppComponent);
+  el: '#options-app',
+  router,
+  components: {
+    App,
+  },
+  render(h) {
+    return h(App);
   },
 });
