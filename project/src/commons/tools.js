cac8e4e87e01b847f0844e9222961dff8af6f5e3
Light <47911535+LightAPIs@users.noreply.github.com>
2021-01-08 20:44:58 +0800

    refactor(commons): 移除部分未使用的方法

--- project/src/commons/tools.js
+++ project/src/commons/tools.js
@@ -8,14 +8,6 @@ const tools = {
    * 防抖延时器值
    */
   debounceTimer: null,
-  /**
-   * 节流延时器值
-   */
-  throttleTimer: null,
-  /**
-   * 节流时间戳值
-   */
-  throttlePrevious: 0,
 
   /**
    * 防抖方法
@@ -55,62 +47,6 @@ const tools = {
     };
 
     return debounced();
-  },
-
-  /**
-   * 节流方法
-   * - 参考：https://github.com/mqyqingfeng/Blog/issues/26
-   * @param {Function} func 函数方法
-   * @param {Number} delay 延迟时间
-   * @param {Object} [options] 参数对象 - leading: false 表示禁用第一次执行; trailing: false 表示禁用停止触发的回调 (两者不能同时设置为 false)
-   */
-  throttle(func, delay, options = {}) {
-    let that, result;
-
-    const throttled = function(...args) {
-      const now = Date.now();
-
-      if (!tools.throttlePrevious && options.leading === false) {
-        // 禁用第一次执行
-        tools.throttlePrevious = now;
-      }
-
-      /** 下次触发 func 剩余的时间 */
-      const remaining = delay - (now - tools.throttlePrevious);
-      that = this;
-      // 如果没有剩余的时间了或者改了系统时间
-      if (remaining <= 0 || remaining > delay) {
-        if (tools.throttleTimer) {
-          clearTimeout(tools.throttleTimer);
-          tools.throttleTimer = null;
-        }
-        tools.throttlePrevious = now;
-        result = func.apply(that, args);
-        if (!tools.throttleTimer) {
-          // 垃圾回收，方便下次执行定时器
-          that = args = null;
-        }
-      } else if (!tools.throttleTimer && options.trailing !== false) {
-        tools.throttleTimer = setTimeout(() => {
-          tools.throttlePrevious = options.leading === false ? 0 : Date.now();
-          tools.throttleTimer = null;
-          result = func.apply(that, args);
-          if (!tools.throttleTimer) {
-            that = args = null;
-          }
-        }, remaining);
-      }
-
-      return result;
-    };
-
-    throttled.cancel = function() {
-      clearTimeout(tools.throttleTimer);
-      tools.throttlePrevious = 0;
-      tools.throttleTimer = null;
-    };
-
-    return throttled();
   },
 
   /**
--- project/src/commons/ui.js
+++ project/src/commons/ui.js
@@ -4,12 +4,6 @@
  * Syntactic sugar
  */
 const ui = {
-  $(id) {
-    return document.getElementById(id);
-  },
-  create(tag) {
-    return document.createElement(tag);
-  },
   get(...arg) {
     return chrome.i18n.getMessage(...arg);
   },
